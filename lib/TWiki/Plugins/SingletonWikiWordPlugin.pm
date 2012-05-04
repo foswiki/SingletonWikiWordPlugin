4e8b8bcd3e04a75b95ecf7b8b97ffe74284bfe9d
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SingletonWikiWordPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SingletonWikiWordPlugin.pm
+++ lib/TWiki/Plugins/SingletonWikiWordPlugin.pm
@@ -12,20 +12,20 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # SingletonWikiWordPlugin implements features as described on
 # http://twiki.org/cgi-bin/view/Codev/?topic=SingletonWikiWord
 
 # =========================
-package TWiki::Plugins::SingletonWikiWordPlugin; 	# change the package name!!!
+package TWiki::Plugins::SingletonWikiWordPlugin;    # change the package name!!!
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $debug
-        $exampleCfgVar
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $debug
+  $exampleCfgVar
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -37,45 +37,46 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
-        &TWiki::Func::writeWarning( "Version mismatch between SingletonWikiWordPlugin and Plugins.pm" );
+    if ( $TWiki::Plugins::VERSION < 1 ) {
+        &TWiki::Func::writeWarning(
+            "Version mismatch between SingletonWikiWordPlugin and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = &TWiki::Func::getPreferencesFlag( "SINGLETONWIKIWORDPLUGIN_DEBUG" );
+    $debug = &TWiki::Func::getPreferencesFlag("SINGLETONWIKIWORDPLUGIN_DEBUG");
 
     # Plugin correctly initialized
-    writeDebug( "- TWiki::Plugins::SingletonWikiWordPlugin::initPlugin( $web.$topic ) is OK" );
+    writeDebug(
+"- TWiki::Plugins::SingletonWikiWordPlugin::initPlugin( $web.$topic ) is OK"
+    );
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    writeDebug( "- X - SingletonWikiWordPlugin::commonTagsHandler( $_[0]$_[2].$_[1] )" );
+    writeDebug(
+        "- X - SingletonWikiWordPlugin::commonTagsHandler( $_[0]$_[2].$_[1] )");
 
     # This is the place to define customized tags and variables
     # Called by sub handleCommonTags, after %INCLUDE:"..."%
-    $_[0] =~ s/(\s+)\.([A-Z]+[a-z]*)/"$1".&TWiki::Func::internalLink("[[$2]]",$web,$web,"",1)/geo;
+    $_[0] =~
+s/(\s+)\.([A-Z]+[a-z]*)/"$1".&TWiki::Func::internalLink("[[$2]]",$web,$web,"",1)/geo;
 
     # do custom extension rule, like for example:
     # $_[0] =~ s/%XYZ%/&handleXyz()/geo;
     # $_[0] =~ s/%XYZ{(.*?)}%/&handleXyz($1)/geo;
 }
 
-sub writeDebug 
-{
-   &TWiki::Func::writeDebug (@_) if $debug;
+sub writeDebug {
+    &TWiki::Func::writeDebug(@_) if $debug;
 }
 
 1;
